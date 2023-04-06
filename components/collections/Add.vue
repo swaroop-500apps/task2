e49eb00bc739c1d0b7fe6f3f77bafaa45b05165a
swaroop-500apps <swaroop.barre@500apps.com>
2023-04-06 22:06:21 +0530

    add lead

---
+++ components/collections/Add.vue
@@ -0,0 +1,191 @@
+<template>
+    <TransitionRoot as="template" :show="open">
+        <Dialog as="div" class="relative z-10" @close="open = false">
+            <div class="fixed inset-0" />
+            <div class="fixed inset-0 overflow-hidden">
+                <div class="absolute inset-0 overflow-hidden">
+                    <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                        <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700"
+                            enter-from="translate-x-full" enter-to="translate-x-0"
+                            leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0"
+                            leave-to="translate-x-full">
+                            <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                                <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                                    <div class="bg-indigo-700 py-6 px-4 sm:px-6">
+                                        <div class="flex items-center justify-between">
+                                            <DialogTitle class="text-base font-semibold leading-6 text-white">Panel title
+                                            </DialogTitle>
+                                            <div class="ml-3 flex h-7 items-center">
+                                                <button type="button"
+                                                    class="rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                                                    @click="open = false">
+                                                    <span class="sr-only">Close panel</span>
+                                                    <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <!-- <div class="mt-1">
+                                            <p class="text-sm text-indigo-300">Lorem, ipsum dolor sit amet consectetur adipisicing elit aliquam ad hic recusandae soluta.</p>
+                                        </div> -->
+                                    </div>
+                                    <div class="relative flex-1 py-6 px-4 sm:px-6">
+                                        <!-- Your content -->
+                                        <form>
+                                            <div class="space-y-12">
+                                                <div class="border-b border-gray-900/10 pb-12">
+                                                    <p class="mt-1 text-sm leading-6 text-gray-600">Add Leads by filling the
+                                                        form below</p>
+                                                </div>
+                                                <div class="border-b border-gray-900/10 pb-12">
+                                                    <div class="mt-10 grid grid-cols-1 gap-y-8 gap-x-6 sm:grid-cols-6">
+                                                        <div class="sm:col-span-3">
+                                                            <label for="name"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">
+                                                                Name</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="namemodel" name="name" id="name"
+                                                                    autocomplete="given-name"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="sm:col-span-3">
+                                                            <label for="source"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">Source</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="sourcemodel" name="source"
+                                                                    id="source" autocomplete="given-source"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="sm:col-span-3">
+                                                            <label for="phonenumber"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">Phone
+                                                                Number</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="phonenumbermodel"
+                                                                    name="phonenumber" id="phonenumber"
+                                                                    autocomplete="given-phonenumber"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="sm:col-span-3">
+                                                            <label for="first_name"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="emailmodel" name="email"
+                                                                    id="email" autocomplete="given-email"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="sm:col-span-3">
+                                                            <label for="status"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">Status</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="statusmodel" name="status"
+                                                                    id="status" autocomplete="given-status"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="sm:col-span-3">
+                                                            <label for="buying"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">Purpose
+                                                                of Buying</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="buyingmodel" name="buying"
+                                                                    id="buying" autocomplete="family-name"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="sm:col-span-3">
+                                                            <label for="address"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">Address</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="addressmodel" name="address"
+                                                                    id="address" autocomplete="given-address"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="sm:col-span-3">
+                                                            <label for="modepayment"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">Mode
+                                                                of Payment</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="modepaymentmodel"
+                                                                    name="modepayment" id="modepayment"
+                                                                    autocomplete="given-modepayment"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="sm:col-span-3">
+                                                            <label for="downpayment"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">Down
+                                                                Payment</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="downpaymentmodel"
+                                                                    name="downpayment" id="downpayment"
+                                                                    autocomplete="given-downpayment"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="sm:col-span-3">
+                                                            <label for="loanrequired"
+                                                                class="block text-sm font-medium leading-6 text-gray-900">Loan
+                                                                Required</label>
+                                                            <div class="mt-2">
+                                                                <input type="text" v-model="loanrequiredmodel"
+                                                                    name="loanrequired" id="loanrequired"
+                                                                    autocomplete="given-loanrequired"
+                                                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                            <div class="mt-6 flex items-center justify-end gap-x-6">
+                                                <button type="submit"
+                                                    class="rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                                    @click.prevent="savemethod()">
+                                                    Save
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
+  
+  
+<script setup lang="ts">
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+
+import { defineEmits, defineProps } from "vue";
+import { ref } from 'vue'
+const { $bus } = useNuxtApp()
+onMounted(() => {
+  console.log('monuted')
+  $bus.$on('openleadSidebar', openleadSidebar)
+})
+const openleadSidebar = () => {
+  open.value = true
+}
+const namemodel = ref('')
+const sourcemodel = ref('')
+const phonenumbermodel = ref('')
+const emailmodel = ref('')
+const statusmodel = ref('')
+const buyingmodel = ref('')
+const addressmodel = ref('')
+const modepaymentmodel = ref('')
+const downpaymentmodel = ref('')
+const loanrequiredmodel = ref('')
+
+</script>
---
+++ components/collections/List.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="px-4 sm:px-6 lg:px-8">
+    <div class="sm:flex sm:items-center">
+      <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+        <button type="button"
+          class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          @click="addlead()">Add Lead</button>
+      </div>
+    </div>
+    <div class="mt-8 flow-root">
+      <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Source</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Phone Number</th>
+                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Email
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Purpose of Buying</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Address</th>
+                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Mode of
+                    payment</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Down Payment</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Loan Required</th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                  <span class="sr-only">Delete</span>
+                </th>
+              </thead>
+              <tbody class="divide-y divide-gray-200 bg-white">
+                <tr v-for="person in people" :key="person.email">
+                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ person.name }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.title }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.email }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.role }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.title }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.email }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.role }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.title }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.email }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.role }}</td>
+                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, {{ person.name
+                    }}</span></a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <collectionsAdd />
+  </div>
+</template>
+  
+<script setup>
+const open = ref(false)
+const { $bus } = useNuxtApp()
+const addlead = () => {
+  $bus.$emit('openleadSidebar')
+}
+</script>
